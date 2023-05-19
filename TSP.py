c9da418b07b36dfb71d40c85cb268bce95fe6594
afick <Alexander.W.Fick.25@dartmouth.edu>
2023-05-18 22:16:09 -0400

    Updating to integrate adjacency matrix calculation

--- TSP.py
+++ TSP.py
@@ -2,6 +2,7 @@ import itertools
 import random
 import sys
 import numpy as np
+import Queue
 
 
 def held_karp(dists):
@@ -74,11 +75,124 @@ def generate_distances(n):
 
     return dists
 
+def expand_boundaries(robot_size, grid):
+        '''
+        Function that creates binary matrix of explorable cells, and expands size of obstacles
+        '''
+        rows = len(grid)
+        cols = len(grid[0])
+        # Initialize a map of the same size that can be fully explored 
+        binary_map = [[False for i in range(cols)] for j in range(rows)]
+        # Calculate the expansion factor
+        # expand = int(ROBOT_SIZE // self.resolution) + SLACK
+
+        # Iterate through each cell
+        for r in range(rows):
+            for c in range(cols):
+                # If there is an obstacle in the map
+                if grid[r][c] != 0: 
+                    # Mark this cell as not explorable, and expand this by the appropriate factor
+                    binary_map[r][c] = True
+                    # for v in range(1, expand+1):
+                    #     if r-v >= 0:
+                    #         binary_map[r-v][c] = True
+                    #     if r+v < self.rows:
+                    #         binary_map[r+v][c] = True
+                    #     if c-v >= 0:
+                    #         binary_map[r][c-v] = True
+                    #     if c+v < self.cols:
+                    #         binary_map[r][c+v] = True
+                        
+        return np.array(binary_map)
+
+def validate(cell, visited):
+    '''
+    Function to validate if a cell can be visited
+    '''
+    row = cell[1]
+    col = cell[0]
+    rows = len(visited)
+    cols = len(visited[0])
+
+    # Check that the cell is in bounds
+    if (row < 0 or col < 0 or row >= rows or col >= cols):
+        return False
+
+    # If the cell is not explorable
+    if (visited[row][col]):
+        return False
+
+    # Otherwise, it can be visited
+    return True
+
+def find_bfs_path(grid, start, end):
+        '''
+        Function to find a path from the provided start and end using BFS
+        '''
+        # 8 Possible row and column movements
+        dRow = [-1, 0, 0, 1, 1, 1, -1, -1]
+        dCol = [0, 1, -1, 0, 1, -1, -1, 1]
+
+        # Create matrix of cells that can and cannot be visited
+        visited = expand_boundaries(grid)
+
+        # Check that the start and goal are reachable
+        if validate(end, visited) == False:
+            exit("goal in obstacle")
+        if validate(start, visited) == False:
+            exit("start in obstacle")
+
+        # Establish queue of nodes we will explore
+        frontier = Queue.Queue()
+        frontier.put(start)
+
+        # Keep track of where a cell came from
+        cell_pointers = {}
+
+        visited[start[0]][start[1]] = True
+
+        # Iterate through the possible nodes
+        while frontier.empty() == False:
+            current = frontier.get()
+
+            if current == end:
+                break
+
+            # Add applicable of the 8 surrounding nodes, keep track of cell origins
+            for i in range(len(dRow)):
+                    next = (current[0] +  dCol[i], current[1] + dRow[i])
+                    if validate(next, visited):
+                        visited[next[0]][next[1]] = True
+                        frontier.put(next)
+                        cell_pointers[next] = current
+
+        # Backtrack through cell connections to find path
+        solution = [] 
+        node = end
+        while node != start:
+            solution.append(node)
+            node = cell_pointers[node]
+        solution.append(start)
+        solution.reverse()
+        return solution
+
 def calc_distances(targets, grid): 
-    pass
+    ''' 
+    Function that creates adjacency matrix for the target nodes 
+    based on the occupancy grid, using A* to find the shortest distance between each node.
+    '''
+    adjacency = np.zeros((len(targets) +1, len(targets) +1))
+    indices = [i for i in range(len(targets))]
+    for start, end in list(itertools.combinations(indices, 2)):
+        length = find_bfs_path(grid, targets[start], targets[end])
+        adjacency[start][end], adjacency[end][start] = length, length
+    return adjacency
 
 def determine_sequence(targets, grid):
     dists = calc_distances(targets, grid)
+
+    time, path = held_karp(dists)
+    return path
 
 def read_distances(filename):
     dists = []
