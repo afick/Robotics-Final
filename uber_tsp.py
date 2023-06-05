8dc3b5d8e96f6f977e35a7ea939ccf5f1df1f553
afick <Alexander.W.Fick.25@dartmouth.edu>
2023-06-05 11:22:04 -0400

    not helping

--- uber_tsp.py
+++ uber_tsp.py
@@ -60,7 +60,7 @@ DEFAULT_ODOM_TOPIC = "robot_0/odom"
 DEFAULT_SCAN_TOPIC = "robot_0/base_scan" # use scan for actual robot
 DEFAULT_MAP_TOPIC = "map"
 
-CUSTOMERS = [(int((6 + 3) / RESOLUTION), int((1 + 3) / RESOLUTION)), (int((1 + 3) / RESOLUTION), int((1 + 3) / RESOLUTION)), (int((2 + 3) / RESOLUTION), int((4 + 3) / RESOLUTION))]
+CUSTOMERS = [(int((6 + 3) / RESOLUTION), int((1 + 3) / RESOLUTION)), (int((1 + 3) / RESOLUTION), int((1 + 3) / RESOLUTION)), (int((6 + 3) / RESOLUTION), int((5 + 3) / RESOLUTION))]
 
 def create_poses(path, res):
     '''
